c399d1b89421b5af311315d56d39cada268934e0
Monk <sabintheworld@gmail.com>
2018-02-21 22:49:08 -0800

    Got rid of unnecessary files
    Gets rid of unused files/tests. Also sets .gitignores to not keep any packages from GitHub/ForgeBox. Docker runs box install upon container initialization now

--- build/tests/test.commandbox.sh
+++
@@ -1,8 +0,0 @@
-cd $APP_DIR
-
-# Test the ability to install an application module
-box install cbi18n
-
-# Test the ability to install and uninstall a commandbox module
-box install CommandBox-Chuck-Norris
-box uninstall Commandbox-Chuck-Norris
